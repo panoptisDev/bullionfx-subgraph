6f02b654f6ca6b53b712975cc99cd56e7e57c5c5
Webmaster <phenix2017pr@gmail.com>
2023-03-13 09:55:16 -0700

    comment using name, symbol bytes

--- subgraphs/exchange/mappings/utils/index.ts
+++ subgraphs/exchange/mappings/utils/index.ts
@@ -1,8 +1,8 @@
 /* eslint-disable prefer-const */
 import { BigInt, BigDecimal, Address, log } from "@graphprotocol/graph-ts";
 import { ERC20 } from "../../generated/BullionFXFactory/ERC20";
-import { ERC20NameBytes } from "../../generated/BullionFXFactory/ERC20NameBytes";
-import { ERC20SymbolBytes } from "../../generated/BullionFXFactory/ERC20SymbolBytes";
+// import { ERC20NameBytes } from "../../generated/BullionFXFactory/ERC20NameBytes";
+// import { ERC20SymbolBytes } from "../../generated/BullionFXFactory/ERC20SymbolBytes";
 import { Factory as FactoryContract } from "../../generated/templates/Pair/Factory";
 
 export let ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
@@ -39,17 +39,17 @@ export function isNullEthValue(value: string): boolean {
 
 export function fetchTokenSymbol(tokenAddress: Address): string {
   let contract = ERC20.bind(tokenAddress);
-  let contractSymbolBytes = ERC20SymbolBytes.bind(tokenAddress);
+  // let contractSymbolBytes = ERC20SymbolBytes.bind(tokenAddress);
 
   let symbolValue = "unknown";
   let symbolResult = contract.try_symbol();
   if (symbolResult.reverted) {
-    let symbolResultBytes = contractSymbolBytes.try_symbol();
-    if (!symbolResultBytes.reverted) {
-      if (!isNullEthValue(symbolResultBytes.value.toHex())) {
-        symbolValue = symbolResultBytes.value.toString();
-      }
-    }
+  //   let symbolResultBytes = contractSymbolBytes.try_symbol();
+  //   if (!symbolResultBytes.reverted) {
+  //     if (!isNullEthValue(symbolResultBytes.value.toHex())) {
+  //       symbolValue = symbolResultBytes.value.toString();
+  //     }
+  //   }
   } else {
     symbolValue = symbolResult.value;
   }
@@ -58,18 +58,18 @@ export function fetchTokenSymbol(tokenAddress: Address): string {
 
 export function fetchTokenName(tokenAddress: Address): string {
   let contract = ERC20.bind(tokenAddress);
-  let contractNameBytes = ERC20NameBytes.bind(tokenAddress);
+  // let contractNameBytes = ERC20NameBytes.bind(tokenAddress);
 
   let nameValue = "unknown";
   let nameResult = contract.try_name();
   if (nameResult.reverted) {
-    let nameResultBytes = contractNameBytes.try_name();
-    log.error(`block:, fetch token name reverted`, []);
-    if (!nameResultBytes.reverted) {
-      if (!isNullEthValue(nameResultBytes.value.toHex())) {
-        nameValue = nameResultBytes.value.toString() || "undefined";
-      }
-    }
+  //   let nameResultBytes = contractNameBytes.try_name();
+  //   log.error(`block:, fetch token name reverted`, []);
+  //   if (!nameResultBytes.reverted) {
+  //     if (!isNullEthValue(nameResultBytes.value.toHex())) {
+  //       nameValue = nameResultBytes.value.toString() || "undefined";
+  //     }
+  //   }
   } else {
     nameValue = nameResult.value;
   }
